1e07f8d9be2588dde250262b91811144c02647e1
MikeGolenko <55780421+MikeGolenko@users.noreply.github.com>
2024-12-24 14:27:35 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+
+
+#include "main.h"
+
+void timer2_init(void){
+	TCCR2A |=(1<<WGM21);
+	TCCR2B |=(1<<CS20)|(1<<CS21)|(1<<CS22);
+	TIMSK2 |=(1<<OCIE2A);
+	OCR2A = 77;//2*1024*(1+38)/8000000=0.01
+}
+
+ISR (TIMER2_COMPA_vect){
+	if(cnt1) cnt1--;
+	if(cnt2) cnt2--;
+	//flagEnc=1;
+	//flagKey = 1;
+}
+
+//uint8_t e,d,s,ths;
+uint16_t a = 0;
+
+int main(void)
+{
+	timer2_init();
+	TM1637_init();
+	DDRD |=(1<<LED);
+	  TM1637_setSegments(TM1637_0, TM1637_DIGITS_COUNT, 0);
+	  TM1637_turnOnAndSetBrightness(1);
+	  sei();
+
+    while (1) 
+    {
+		if(cnt2==0){PORTD ^= (1<<LED);
+			flag_dot = !flag_dot;
+			cnt2=100;}
+		if(cnt1==0){
+			a++;
+			TM1637_displayNumber(a);
+			if(a==9999){a=0;};				
+			cnt1=10;
+			}
+
+    }
+}
+
---
+++ main.h
@@ -0,0 +1,23 @@
+
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "tm1637.h"
+
+#define LED 4
+
+volatile uint8_t cnt1,cnt2;
+
+
+
+
+
+#endif /* MAIN_H_ */
